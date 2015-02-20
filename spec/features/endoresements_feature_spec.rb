51d4f64dd57289eaaab5fbfc502a9ac89340b4f0
Andy <andygnewman@gmail.com>
2015-02-20 15:01:58 +0000

    tests for adding endorsements passed

--- spec/features/endoresements_feature_spec.rb
+++ spec/features/endoresements_feature_spec.rb
@@ -7,9 +7,22 @@ feature 'endorsing reviews' do
     kfc.reviews.create(rating: 3, thoughts: 'finger lickin')
   end
 
-  scenario 'a user can endorse a review, which updates the endorsement count' do
+  scenario 'a review has a alink to endorse a review' do
+    visit '/restaurants'
+    expect(page).to have_link('Endorse this KFC review')
+  end
+
+  scenario 'a review display an endorsement associated with it' do
     visit '/restaurants'
     click_link 'Endorse this KFC review'
+    expect(page).to have_content('1 endorsement')
+  end
+
+  scenario 'a review displays multiple endorsements associated with it' do
+    visit '/restaurants'
+    click_link 'Endorse this KFC review'
+    click_link 'Endorse this KFC review'
+    expect(page).to have_content('2 endorsements')
   end
 
 end
